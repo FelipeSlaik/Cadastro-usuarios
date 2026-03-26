26317f618b5a5cd630e31de67922a8322dfc2ea4
Felipe Santos <felipeslaik@gmail.com>
2026-03-25 21:01:22 -0300

    usando um pouco do tailwind para estilizar o formulario

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -18,30 +18,40 @@ function Cadastro() {
     }
   }
   return (
-    <form onSubmit={cadastro} className="flex fle">
-      <input
-        type="text"
-        placeholder="Digite seu nome"
-        onChange={(e) => setNomeDigitado(e.target.value)}
-        value={nomeDigitado}
-      />
+    <div className="w-full  min-h-screen flex justify-center items-center bg-gray-200">
+      <form
+        onSubmit={cadastro}
+        className="flex flex-col gap-4 bg-lime-600 p-6 rounded-2xl w-80"
+      >
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          onChange={(e) => setNomeDigitado(e.target.value)}
+          value={nomeDigitado}
+        />
 
-      <input
-        type="email"
-        placeholder="Digite seu E-mail"
-        onChange={(e) => setEmailDigitado(e.target.value)}
-        value={emailDigitado}
-      />
+        <input
+          type="email"
+          placeholder="Digite seu E-mail"
+          onChange={(e) => setEmailDigitado(e.target.value)}
+          value={emailDigitado}
+        />
 
-      <input
-        type="password"
-        placeholder="Digite sua senha"
-        onChange={(e) => setSenha(e.target.value)}
-        value={senha}
-      />
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={(e) => setSenha(e.target.value)}
+          value={senha}
+        />
 
-      <button type="submit">Enviar</button>
-    </form>
+        <button
+          className="cursor-pointer bg-amber-700 w-40 p-1.5 rounded-2xl mt-1.5"
+          type="submit"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
   );
 }
 
